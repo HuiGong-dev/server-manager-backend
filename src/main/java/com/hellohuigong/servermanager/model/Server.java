1996d7e57d19f0daea3c59c834b0f48dc8f3cdec
HuiGong-dev <hellohuigong@gmail.com>
2023-05-06 14:21:01 +0200

    init backend

---
+++ src/main/java/com/hellohuigong/servermanager/model/Server.java
@@ -0,0 +1,13 @@
+package com.hellohuigong.servermanager.model;
+
+import com.hellohuigong.servermanager.enumeration.Status;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
