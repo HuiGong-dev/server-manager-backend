489a2b8fd740b76185a26d8dd325486c65da7863
HuiGong-dev <hellohuigong@gmail.com>
2023-05-06 18:25:31 +0200

    connect to remote mysql server

--- src/main/java/com/hellohuigong/servermanager/resource/ServerResource.java
+++ src/main/java/com/hellohuigong/servermanager/resource/ServerResource.java
@@ -1,31 +1,95 @@
 package com.hellohuigong.servermanager.resource;
 
+import com.hellohuigong.servermanager.enumeration.Status;
 import com.hellohuigong.servermanager.model.Response;
+import com.hellohuigong.servermanager.model.Server;
 import com.hellohuigong.servermanager.service.implementation.ServerServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 
 import static java.time.LocalDateTime.now;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 @RestController
 @RequestMapping("/server")
 @RequiredArgsConstructor
 public class ServerResource {
     private final ServerServiceImpl serverService;
+
+    @GetMapping("/list")
     public ResponseEntity<Response> getServers(){
         return ResponseEntity.ok(Response.builder()
                 .timeStamp(now())
-                .data(Map.of("servers", serverService.list(30) ))
+                .data(Map.of("servers", serverService.list(30)))
                 .message("servers retrieved")
                 .status(HttpStatus.OK)
                 .statusCode(HttpStatus.OK.value())
                 .build()
         );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(Response.builder()
+                .timeStamp(now())
+                .data(Map.of("server", server))
+                .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
+                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .build()
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+        return ResponseEntity.ok(
+                Response.builder()
+                .timeStamp(now())
+                .data(Map.of("server", serverService.create(server)))
+                .message("Server created")
+                .status(HttpStatus.CREATED)
+                .statusCode(HttpStatus.CREATED.value())
+                .build()
+        );
+    }
+
+    @GetMapping("/ping/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(Response.builder()
+                .timeStamp(now())
+                .data(Map.of("server", serverService.get(id)))
+                .message("Server retrieved")
+                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(Response.builder()
+                .timeStamp(now())
+                .data(Map.of("server deleted", serverService.delete(id)))
+                .message("Server deleted")
+                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .build()
+        );
+    }
+
+    @GetMapping(path = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
+    public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home")+"/Downloads/images/"+fileName));
     }
 
 
