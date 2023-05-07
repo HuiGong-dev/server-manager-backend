662494bddf9676c31e91978646ab8832951f1b2e
HuiGong-dev <hellohuigong@gmail.com>
2023-05-07 15:18:24 +0200

    handle cors and set timeout to 1 sec

--- src/main/java/com/hellohuigong/servermanager/service/implementation/ServerServiceImpl.java
+++ src/main/java/com/hellohuigong/servermanager/service/implementation/ServerServiceImpl.java
@@ -36,7 +36,7 @@ public class ServerServiceImpl implements ServerService {
         log.info("Pinging server IP: {}", ipAddress);
         Server server = serverRepo.findByIpAddress(ipAddress);
         InetAddress address = InetAddress.getByName(ipAddress);
-        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        server.setStatus(address.isReachable(1000) ? Status.SERVER_UP : Status.SERVER_DOWN);
         serverRepo.save(server);
         return server;
     }
